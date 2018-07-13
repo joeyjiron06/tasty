b6f7fca22aec0574103b31f4bf98715d38951f0b
Joey Jiron <joey.jiron@am.sony.com>
2018-07-13 11:29:15 -0700

    wip

--- src/routes/recipe.js
+++ src/routes/recipe.js
@@ -2,37 +2,7 @@ import React, { Component } from 'react';
 import { Typography, Divider, Icon } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 
-const styles = theme => ({
-  page: {
-    width: '100vw',
-    height: '100vh',
-    background: theme.palette.primary.main
-  },
-  infoContainer: {
-    border: 'solid 1px red'
-  },
-  title: {
-    letterSpacing: 2.8
-  },
-  subtitleContainer: {},
-  chip: {
-    padding: '4px 10px',
-    borderRadius: 6,
-    border: 'solid 1px rgba(255, 255, 255,0.7)'
-  },
-  directionsList: {},
-  directionStep: {
-    display: 'flex',
-    marginBottom: 20
-  },
-  directionNumber: {
-    minWidth: 40,
-    display: 'inline'
-  },
-  directionText: {
-    display: 'inline'
-  }
-});
+
 
 class RecipePage extends Component {
   render() {
@@ -41,28 +11,28 @@ class RecipePage extends Component {
       <div className={classes.page}>
         <div className={classes.infoContainer}>
           <Typography variant="display3" className={classes.title}>
-            Bean Tostadas
+            Tacos
           </Typography>
 
           <Divider />
 
-          <div classes={classes.subtitleContainer}>
-            <div>
-              <span>
+          <div className={classes.subtitleContainer}>
+            <div className={classes.timeAndServicesContainer}>
+              <div className={classes.timeContainer}>
                 <Icon>access_time</Icon>
-                <span>1 hour</span>
-              </span>
+                <div>1 hour</div>
+              </div>
 
-              <span>
+              <div>
                 <Icon>add</Icon>
                 <Icon>people_outline</Icon>
                 <Icon>remove</Icon>
-              </span>
+              </div>
             </div>
 
-            <div>
-              <span className={classes.chip}>Vegan</span>
-              <span className={classes.chip}> Breakfast</span>
+            <div className={classes.tagsContainer}>
+              <div className={classes.chip}>Vegan</div>
+              <div className={classes.chip}> Breakfast</div>
             </div>
           </div>
 
@@ -84,9 +54,9 @@ class RecipePage extends Component {
             <Typography variant="title">Directions</Typography>
             <div className={classes.directionsList}>
               {`Marinate the cauliflower. Cut up cauliflower and marinate with olive oil (enough to coat it on the outside) then add chipotle powder, onion powder, a whole lime and salt and pepper. Let it marinade for as much time as you have or while you perepare the rest.
-                Roast the cauliflower in the oven at 425℉ for 35-45min checking on it periodically. 
+                Roast the cauliflower in the oven at 425℉ for 35-45min checking on it periodically.
                 Start boiling water for the next step.
-                After twenty minutes of roasting the cauliflower add your corn to boiling water and let it cook for 15 minutes. 
+                After twenty minutes of roasting the cauliflower add your corn to boiling water and let it cook for 15 minutes.
                 Immediately after putting the corn on start making cilantro lime rice.
                 Once the rice and corn is done, check on your cauliflower.
                 Serve on your tortillas and garnish with cilantro, jalapenos (fresh or pickled), onions and lime`
@@ -108,5 +78,57 @@ class RecipePage extends Component {
     );
   }
 }
+
+const styles = theme => ({
+  page: {
+    width: '100vw',
+    height: '100vh',
+    background: theme.palette.primary.main
+  },
+  infoContainer: {
+    border: 'solid 1px red'
+  },
+  title: {
+    letterSpacing: 2.8
+  },
+  subtitleContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    flexWrap: 'wrap',
+    padding: '20px 0'
+  },
+  timeAndServicesContainer: {
+    display: 'flex',
+    alignItems:'center',
+    flexShrink: 0,
+    marginRight: 40,
+  },
+  timeContainer: {
+    display: 'flex',
+    alignItems:'center'
+  },
+  tagsContainer: {
+    flexShrink: 0,
+    display:'flex',
+  },
+  chip: {
+    border: 'solid 1px rgba(255, 255, 255,0.7)',
+    padding: '10px 14px',
+    marginRight: 10,
+    borderRadius: 100
+  },
+  directionsList: {},
+  directionStep: {
+    display: 'flex',
+    marginBottom: 20
+  },
+  directionNumber: {
+    minWidth: 40,
+    display: 'inline'
+  },
+  directionText: {
+    display: 'inline'
+  }
+});
 
 export default withStyles(styles)(RecipePage);
