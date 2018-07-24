b5f2a486a752adc16ed646c2dded2fb2f838a62e
Joey Jiron <joeyjiron06@gmail.com>
2018-07-24 11:53:23 -0700

    added search input to homepage

--- src/components/toolbar.js
+++ src/components/toolbar.js
@@ -1,20 +1,57 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
 import { withStyles } from '@material-ui/core/styles';
-import { Typography, IconButton, Icon } from '@material-ui/core';
+import { Typography, IconButton, Icon, TextField } from '@material-ui/core';
 
 class Toolbar extends Component {
+  state = {
+    showSearchField: false
+  };
   handleRouteClicked = name => event => {
     this.props.history.push(name);
+  };
+  handleSearchChanged = event => {};
+
+  handleSearchIconClicked = event => {
+    const showSearchField = !this.state.showSearchField;
+
+    this.setState(
+      {
+        showSearchField
+      },
+      () => {
+        if (showSearchField) {
+          this.searchInputRef.focus();
+        } else {
+          this.searchInputRef.blur();
+        }
+      }
+    );
   };
 
   render() {
     const { classes, history } = this.props;
+    const { showSearchField } = this.state;
+
     return (
       <div className={classes.toolbar}>
-        <IconButton>
-          <Icon>search</Icon>
-        </IconButton>
+        <div>
+          <IconButton onClick={this.handleSearchIconClicked}>
+            <Icon>search</Icon>
+          </IconButton>
+          <TextField
+            inputRef={ref => (this.searchInputRef = ref)}
+            onChange={this.handleSearchChanged}
+            className={`${classes.searchInput}
+            
+            ${
+              showSearchField
+                ? classes.searchInputShown
+                : classes.searchInputHidden
+            }
+            `}
+          />
+        </div>
 
         <div className={classes.routes}>
           <Typography
@@ -57,6 +94,15 @@ const styles = theme => ({
   },
   routeSelected: {
     borderBottomColor: theme.palette.secondary.main
+  },
+  searchInput: {
+    transition: 'width 250ms ease-out'
+  },
+  searchInputShown: {
+    width: 200
+  },
+  searchInputHidden: {
+    width: 0
   }
 });
 
