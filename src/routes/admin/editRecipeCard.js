ccefcafbeccc2a6eaba06a7a1d887e2195863346
Joey Jiron <joeyjiron06@gmail.com>
2018-09-10 15:54:56 -0700

    added edit recipe card

--- src/routes/admin/editRecipeCard.js
+++ src/routes/admin/editRecipeCard.js
@@ -2,7 +2,26 @@ import React from 'react';
 
 const EditRecipeCard = ({ recipe }) => (
   <div>
-    <input placeholder="Name of Recipe" value={recipe.title} />
+    <input placeholder='Name of Recipe' value={recipe.title} />
+    <input placeholder='How many does it serve?' value={recipe.serves} />
+    <input placeholder='How long does it take?' value={recipe.duration} />
+    <input placeholder='Image url' value={recipe.image} />
+    <img alt='recipe preview' src={recipe.image} />
+
+    {recipe.tags.map(tag => (
+      <input placeholder='Tag name' value={tag} />
+    ))}
+    <button>ADD TAG</button>
+
+    {recipe.ingredients.map(ingredient => (
+      <input placeholder='Ingredient' value={ingredient} />
+    ))}
+    <button>ADD INGREDIENT</button>
+
+    {recipe.directions.map(direction => (
+      <input placeholder='Direction' value={direction} />
+    ))}
+    <button>ADD DIRECTION</button>
   </div>
 );
 
--- src/routes/admin/editRecipeCard.test.js
+++ src/routes/admin/editRecipeCard.test.js
@@ -1,20 +1,52 @@
 import React from 'react';
 import EditRecipeCard from './editRecipeCard';
-import { render, fireEvent } from 'react-testing-library';
+import { render } from 'react-testing-library';
 
 describe('<EditRecipeCard />', () => {
   const recipe = {
     title: 'Soba Noodles',
     image: 'http://image.com',
+    serves: 2,
+    duration: 60,
     tags: ['Dinner', 'Noodle', 'Japanese'],
     ingredients: ['1 lime', '1 cup of noodles'],
     directions: ['boil noodles', 'serve hot']
   };
 
   it('should render the recipe received from props', () => {
-    const { getByPlaceholderText } = render(<EditRecipeCard recipe={recipe} />);
-    expect(getByPlaceholderText(/name of recipe/i)).toHaveTextContent(
-      recipe.title
+    const {
+      getByPlaceholderText,
+      getAllByPlaceholderText,
+      getByAltText,
+      getByText
+    } = render(<EditRecipeCard recipe={recipe} />);
+
+    expect(getByPlaceholderText(/name of recipe/i).value).toBe(recipe.title);
+    expect(getByPlaceholderText(/how many does it serve\?/i).value).toBe(
+      recipe.serves + ''
+    );
+    expect(getByPlaceholderText(/how long does it take\?/i).value).toBe(
+      recipe.duration + ''
+    );
+    expect(getByPlaceholderText(/image url/i).value).toBe(recipe.image);
+    expect(getByAltText(/recipe preview/i)).toHaveAttribute(
+      'src',
+      recipe.image
+    );
+
+    expect(getByText(/add tag/i)).toBeInTheDocument();
+    getAllByPlaceholderText(/tag name/i).forEach((el, index) =>
+      expect(el.value).toBe(recipe.tags[index])
+    );
+
+    expect(getByText(/add ingredient/i)).toBeInTheDocument();
+    getAllByPlaceholderText(/ingredient/i).forEach((el, index) =>
+      expect(el.value).toBe(recipe.ingredients[index])
+    );
+
+    expect(getByText(/add direction/i)).toBeInTheDocument();
+    getAllByPlaceholderText(/direction/i).forEach((el, index) =>
+      expect(el.value).toBe(recipe.directions[index])
     );
   });
   it('should not render values when recipe values are empty');
